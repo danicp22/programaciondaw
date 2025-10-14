60465dedd8e97c5753c9a03982bebd931ad780da
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-10-14 18:10:38 +0200

    1

--- 001-Identificacion de los elementos de un programa/PYTHON/010.tipos de datos.py
+++ 001-Identificacion de los elementos de un programa/PYTHON/010.tipos de datos.py
@@ -1,5 +1,5 @@
 
     edad = 17 #tipo de dato entero
     altura = 1.70 #tipo de dato decimal
-    nombre = "Andres Ramirez"#variable de tipo cadenal
+    nombre = "Daniel Calve"#variable de tipo cadenal
     mentiroso = True #tipo booleano
