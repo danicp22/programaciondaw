581ea10b4d227bacc554947182c671220c596e2f
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-11-18 12:20:47 +0100

    1

---
+++ 006-Aplicación de las estructuras de almacenamiento/002-Creación de matrices (arrays)/005-acciones del menu.py
@@ -0,0 +1,12 @@
+menu = []
+
+while True:
+    print("Opciones:")
+    print("1.-Introducir nueva comida en el menu")
+    print("2.-Listar comidas en el menu")
+    opcion = input("Selecciona una opcion")
+    comida = input("Introduce el nombre de la comida: ")
+    menu.append(comida)
+    print("Tu comida hasta el momento es: ")
+    for elemento in menu:
+        print(elemento)
