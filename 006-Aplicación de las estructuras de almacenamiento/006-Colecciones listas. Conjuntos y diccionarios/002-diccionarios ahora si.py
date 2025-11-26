580005877e1eeff17fe8be8bbb04d07e6ae05df7
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-11-26 08:56:39 +0100

    1

---
+++ 006-Aplicación de las estructuras de almacenamiento/006-Colecciones listas. Conjuntos y diccionarios/002-diccionarios ahora si.py
@@ -0,0 +1,15 @@
+# Esto sigue siendo una lista
+persona = [
+    "Daniel",
+    "Calve Pardo",
+    "daniel@example.com",
+    18
+]
+
+# Ahora en formato diccionario
+persona = {
+    "nombre":"Daniel",
+    "apellidos":"Calve Pardo",
+    "correo":"daniel@example.com",
+    "edad":18
+}
