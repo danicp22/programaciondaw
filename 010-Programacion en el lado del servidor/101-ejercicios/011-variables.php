6e4a7e2892f79d5b53f1f93b07047db3a199bf1b
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-12-02 11:12:15 +0100

    1

---
+++ 010-Programacion en el lado del servidor/101-ejercicios/011-variables.php
@@ -0,0 +1,6 @@
+<?php
+    $edad = 18; // Las variables se declaran con dolar
+    echo $edad; // Podemos hacer echo de variables
+    echo "<br>"; // Salto de linea
+    $edad = 19; // Podemos cambiar el valor de una variable 
+    echo $edad; 
