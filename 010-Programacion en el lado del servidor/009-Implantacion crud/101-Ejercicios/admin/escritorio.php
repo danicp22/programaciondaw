4300476e7cf0c364b1d06bdeabecd4218c651b4f
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-12-12 10:37:11 +0100

    1

--- 010-Programacion en el lado del servidor/009-Implantacion crud/101-Ejercicios/admin/escritorio.php
+++ 010-Programacion en el lado del servidor/009-Implantacion crud/101-Ejercicios/admin/escritorio.php
@@ -1,5 +1,3 @@
-<!--http://..../basesdedatosdamdaw2526/008-Proyectos/003-Panel%20de%20control/101-Ejercicios/aplicacion/admin/escritorio.php -->
-
 <!doctype html>
 <html lang="es">
 	<head>
@@ -14,9 +12,14 @@
     </nav>
     <main>
     	<?php
+      	// Esto se conoce como router (enrutador) /////////////
       	if(isset($_GET['accion'])){
         	if($_GET['accion'] == "nuevo"){
           	include "inc/create/formulario.php";
+          }else if($_GET['accion'] == "eliminar"){ // Defino la acción eliminar
+          	include "inc/delete/eliminar.php";		// En ese caso incluyo eliminar.php
+          }else if($_GET['accion'] == "editar"){ // Defino la acción editar
+          	include "inc/update/formularioactualizar.php";		// En ese caso incluyo el formulario de la edicion.php
           }
         }else{
       		include "inc/read/leer.php"; 
