912ec2cee48c028965c0d5ae0fbcd2a04409398d
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-12-12 08:40:46 +0100

    1

---
+++ 010-Programacion en el lado del servidor/009-Implantacion crud/101-Ejercicios/admin/escritorio.php
@@ -0,0 +1,28 @@
+<!--http://..../basesdedatosdamdaw2526/008-Proyectos/003-Panel%20de%20control/101-Ejercicios/aplicacion/admin/escritorio.php -->
+
+<!doctype html>
+<html lang="es">
+	<head>
+  	<title>El jocarsa - Panel de control</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/estilo.css">
+  </head>
+  <body>
+  	<nav>
+    	<button>Noticias</button>
+      <button>Autores</button>
+    </nav>
+    <main>
+    	<?php
+      	if(isset($_GET['accion'])){
+        	if($_GET['accion'] == "nuevo"){
+          	include "inc/create/formulario.php";
+          }
+        }else{
+      		include "inc/read/leer.php"; 
+        }
+      ?>
+      <a href="?accion=nuevo" id="nuevo">+</a>
+    </main>
+  </body>
+</html>
